aa0e8dfcf30d97c899b7d9dd5a813990dbc10760
kateee505 <150558093+kateee505@users.noreply.github.com>
2026-04-26 19:14:06 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("btn").addEventListener("click", calculate);
+
+function calculate() {
+    const price = Number(document.getElementById("bike").value);
+    const days = Number(document.getElementById("days").value);
+    const result = document.getElementById("result");
+
+    if (!days || days <= 0) {
+        result.innerHTML = "❌ Lūdzu ievadi pareizu dienu skaitu!";
+        result.style.color = "red";
+        return;
+    }
+
+    let total = price * days;
+
+    if (days >= 7) {
+        total *= 0.85;
+    }
+
+    result.innerHTML = `💰 Kopējā cena: ${total.toFixed(2)} €`;
+    result.style.color = "green";
+}
